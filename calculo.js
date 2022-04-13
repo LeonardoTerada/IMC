8e687d76ca3faacfb68f26f033c4a7728b79ff28
LeonardoTerada <teradaleo@gmail.com>
2022-04-13 15:40:51 -0300

    mexendo html com js

--- calculo.js
+++ calculo.js
@@ -1,11 +1,20 @@
 
 function calcule(){
 
-
-let peso = document.querySelector("#peso").value;
-let altura = document.querySelector('#altura').value;
+const nome = document.querySelector('#nome').value;
+const peso = document.querySelector("#peso").value;
+const altura = document.querySelector('#altura').value;
 let pesoconvertido = parseFloat(peso)
 let alturaconvertido = parseFloat(altura)
-let soma = peso/(alturaconvertido * alturaconvertido);
-console.log(soma)
+let soma = (peso/(alturaconvertido * alturaconvertido)).toFixed(2);
+console.log(soma);
+
+
+
+ 
+
+
+
+let resposta = document.querySelector('#mensagem');
+resposta.textContent = ('Olá , ' + nome + ' seu IMC é de : ' + soma );
 }
